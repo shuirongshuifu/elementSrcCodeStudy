a81fbdf3ae0294e3949c88cf90256962fbad211a
lss <lss13611833316@163.com>
2022-12-04 14:39:29 +0800

    '增加内部tab可拖拽功能'

--- src/layOut/rightMiddleTags/index.vue
+++ src/layOut/rightMiddleTags/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="rightMiddleTags">
+  <div class="rightMiddleTags" id="rightMiddleTags">
     <!-- 首页 -->
     <my-tag
       class="selfClass"
@@ -26,6 +26,8 @@
 </template>
 
 <script>
+// 引入sortablejs插件，拖拽主要靠的是这个插件
+import Sortable from "sortablejs";
 import { mapState } from "vuex";
 export default {
   computed: {
@@ -38,6 +40,9 @@ export default {
     $route(newRoute, oldRoute) {
       this.addTags(newRoute, oldRoute);
     },
+  },
+  mounted() {
+    this.columnDropInit();
   },
   methods: {
     addTags(newRoute, oldRoute) {
@@ -58,6 +63,29 @@ export default {
       this.$store.dispatch("tags/delete_view", { name: item.name, isCurrent });
       // console.log("删除", isCurrent);
     },
+    columnDropInit() {
+      // return
+      //第一步，获取拖动容器
+      var wrap = document.getElementById("rightMiddleTags");
+      //第二步，设置拖动项的拖动规则
+      var rules = {
+        animation: 500, // 拖动时的元素的位置变化的动画时长，
+        //拖动结束后的回调函数
+        onEnd: function (event) {
+          // console.log("参数是拖动事件对象", event);
+          //获取拖动后容器中的每一项的位置排序
+          var arr = sortable.toArray();
+          // console.log("位置排序", arr);
+        },
+      };
+      //第三步，初始化 --> 给拖动容器添加拖动规则
+      var sortable = Sortable.create(wrap, rules);
+      /**
+       * 插件自带的方法：
+       *      1. sortable.toArray() 获取序列化后的每个item元素的id属性的数组
+       *      2. Sortable.create(wrap, rules) 给拖动容器添加拖动规则
+       * */
+    },
   },
 };
 </script>
@@ -71,11 +99,16 @@ export default {
   align-items: center;
   box-sizing: border-box;
   padding: 0 12px;
+  overflow-x: auto;
   .selfClass {
     margin-right: 12px;
     cursor: pointer;
+    transition: all 0.3s;
   }
   .selfClass:hover {
+    color: #67c23a;
+  }
+  .selfClass:active {
     color: #67c23a;
   }
 }
